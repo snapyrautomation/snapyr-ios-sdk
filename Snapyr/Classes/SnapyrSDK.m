3fe2ca1822135dd3b6df14c340ffd6d903f28d9a
boneill42 <bone@alumni.brown.edu>
2021-04-04 10:12:38 -0400

    rename of event, should be internal so we can see it attacehd to a user profile

--- Snapyr/Classes/SnapyrSDK.m
+++ Snapyr/Classes/SnapyrSDK.m
@@ -336,7 +336,7 @@ NSString *const SnapyrBuildKeyV2 = @"SnapyrBuildKeyV2";
 
 - (void)pushNotificationReceived:(NSDictionary *)info
 {
-    [self track:@"snapyr.hidden.pushNotificationReceived" properties:info];
+    [self track:@"snapyr.internal.pushNotificationReceived" properties:info];
 }
 
 
